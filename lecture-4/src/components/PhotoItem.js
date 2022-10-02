eb45222013aed8513d8e6748d1463d6d181ca961
choi2601 <byeonghyeonchoe55@gmail.com>
2022-10-02 14:00:13 +0900

    이미지 사이즈 고정으로 두어 layout shift 막기

---
+++ lecture-4/src/components/PhotoItem.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import styled from 'styled-components';
+import { useDispatch } from 'react-redux';
+import { showModal } from '../redux/imageModal';
+
+function PhotoItem({ photo: { urls, alt } }) {
+  const dispatch = useDispatch();
+
+  const openModal = () => {
+    dispatch(showModal({ src: urls.full, alt }));
+  };
+
+  return (
+    <ImageWrap>
+      <Image src={urls.small + '&t=' + new Date().getTime()} alt={alt} onClick={openModal} />
+    </ImageWrap>
+  );
+}
+
+const ImageWrap = styled.div`
+  position: relative;
+  width: 100%;
+  padding-bottom: 56.25%;
+`;
+
+const Image = styled.img`
+  cursor: pointer;
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+`;
+
+export default PhotoItem;
